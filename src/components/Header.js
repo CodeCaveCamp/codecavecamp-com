58256dc00f7a211a16408cd4a6ce4ff533b86511
“CodeCaverAmy” <amyplant@me.com>
2018-02-13 17:18:16 -0600

    Build animated navigation for mobile devices

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,11 @@ import { startLogin, startLogout } from '../actions/auth';
 export class Header extends React.Component {
 
     onToggle = (e) => {
-        const classList = e.target.className.includes('bar') ? e.target.parentNode.classList : e.target.classList; 
-        classList.toggle('change');
+        const hamburger = e.target.className.includes('bar') ? e.target.parentNode : e.target;
+        const bars = hamburger.classList; 
+        const navigation = hamburger.nextSibling.classList;
+        navigation.toggle('showMenu');
+        bars.toggle('x');
     };
 
     render () {
@@ -23,10 +26,19 @@ export class Header extends React.Component {
                             <NavLink className="header__link" to='/Contact' activeClassName='is-active'>Contact</NavLink>
                             <button className="button" onClick={startLogin}>Login</button>
                         </div>
-                        <div className="header__navigation-mobile mobile" onClick={this.onToggle}>
-                            <div className="bar"></div>
-                            <div className="bar"></div>
-                            <div className="bar"></div>
+                        <div className="header__navigation-mobile mobile">
+                            <div className="hamburger"  onClick={this.onToggle}>
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                                <div className="bar"></div>
+                            </div>
+                            <div className="navlinks-mobile">
+                                <NavLink className="header__link" to='/' exact={true} activeClassName='is-active'>Courses</NavLink>
+                                <NavLink className="header__link" to='/Team' activeClassName='is-active'>Our Team</NavLink>
+                                <NavLink className="header__link" to='/Community' activeClassName='is-active'>Community</NavLink>
+                                <NavLink className="header__link" to='/Contact' activeClassName='is-active'>Contact</NavLink>
+                                <NavLink className="header__link" to='/'>Login</NavLink>
+                            </div>
                         </div>
                     </div>
                 </div>
