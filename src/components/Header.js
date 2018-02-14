78acc02aa8e268a5e583bb4657120664a03a75c9
“CodeCaverAmy” <amyplant@me.com>
2018-02-13 20:21:34 -0600

    Refactor mobile navigation so it slides in from the top

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,39 +8,35 @@ export class Header extends React.Component {
     onToggle = (e) => {
         const hamburger = e.target.className.includes('bar') ? e.target.parentNode : e.target;
         const bars = hamburger.classList; 
-        const navigation = hamburger.nextSibling.classList;
-        navigation.toggle('showMenu');
+        console.log(bars);
+        document.getElementById('nav-mobile').classList.toggle('showMenu');
         bars.toggle('x');
     };
 
     render () {
         return (
             <header className="header">
-                <div className="content-container">
-                    <div className="header__content">
-                        <Link className="header__logo" to='/dashboard'><img src="/images/logo_header.svg" /></Link>
-                        <div className="header__navigation-desktop desktop">
-                            <NavLink className="header__link" to='/' exact={true} activeClassName='is-active'>Courses</NavLink>
-                            <NavLink className="header__link" to='/Team' activeClassName='is-active'>Our Team</NavLink>
-                            <NavLink className="header__link" to='/Community' activeClassName='is-active'>Community</NavLink>
-                            <NavLink className="header__link" to='/Contact' activeClassName='is-active'>Contact</NavLink>
-                            <button className="button" onClick={startLogin}>Login</button>
-                        </div>
-                        <div className="header__navigation-mobile mobile">
-                            <div className="hamburger"  onClick={this.onToggle}>
-                                <div className="bar"></div>
-                                <div className="bar"></div>
-                                <div className="bar"></div>
-                            </div>
-                            <div className="navlinks-mobile">
-                                <NavLink className="header__link" to='/' exact={true} activeClassName='is-active'>Courses</NavLink>
-                                <NavLink className="header__link" to='/Team' activeClassName='is-active'>Our Team</NavLink>
-                                <NavLink className="header__link" to='/Community' activeClassName='is-active'>Community</NavLink>
-                                <NavLink className="header__link" to='/Contact' activeClassName='is-active'>Contact</NavLink>
-                                <NavLink className="header__link" to='/'>Login</NavLink>
-                            </div>
-                        </div>
+                <div className="header__content">
+                    <Link className="header__logo" to='/'><img src="/images/logo_header.svg" /></Link>
+                    <div className="header__navigation-desktop desktop">
+                        <NavLink className="header__link" to='/' exact={true} activeClassName='is-active'>Courses</NavLink>
+                        <NavLink className="header__link" to='/Team' activeClassName='is-active'>Our Team</NavLink>
+                        <NavLink className="header__link" to='/Community' activeClassName='is-active'>Community</NavLink>
+                        <NavLink className="header__link" to='/Contact' activeClassName='is-active'>Contact</NavLink>
+                        <button className="button" onClick={startLogin}>Login</button>
                     </div>
+                    <div className="hamburger mobile"  onClick={this.onToggle}>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                    </div>
+                </div>
+                <div id="nav-mobile" className="navlinks-mobile mobile">
+                    <NavLink className="header__link" to='/' exact={true} activeClassName='is-active'>Courses</NavLink>
+                    <NavLink className="header__link" to='/Team' activeClassName='is-active'>Our Team</NavLink>
+                    <NavLink className="header__link" to='/Community' activeClassName='is-active'>Community</NavLink>
+                    <NavLink className="header__link" to='/Contact' activeClassName='is-active'>Contact</NavLink>
+                    <NavLink className="header__link" to='/'>Login</NavLink>
                 </div>
             </header>
         );
