a07be468f1790f207d3d0f1c01189c32660afc8a
Amy Plant <amy.plant@code-ninja.co>
2018-02-14 16:37:18 -0600

    Style Hero for both Mobile and Desktop

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,32 +2,31 @@ import React from 'react';
 
 const Hero = () => (
     <div className='hero'>
-        <div className='content-container'>
-            <div className='hero__container'>
-                <div className='hero__message'>
-                    <div className='hero__message__header'>
-                        <img src='/images/code-cave-camp-logo.svg' />
-                        <div className='hero__message__header__callout'>
-                            <h1>Ready</h1>
-                            <h1>Set</h1>
-                            <h1>Code</h1>
-                        </div>
-                    </div>
-                    <div className='hero__message__body'>
-                        <p>Do you have the desire to build, disassemble, question, problem solve and conquer challenges that others cannot?</p>
-                        <p>Sign up for our challenge, receive a free Code Cavers notebook, and we will help get you to the starting line.</p>
-                    </div>
+        <div className='hero__header-container'>
+            <div className='hero__header'>
+                <div className='hero__header-logo'>
+                    <img className='hero__header-logo__image' src='/images/code-cave-camp-logo.svg' />
                 </div>
-
-                <form className='hero__form'>
-                    <div className='hero__form__inputs'>
-                        <input className='text-input' type="text" placeholder="Name" />
-                        <input className='text-input' type="text" placeholder="Email" />
-                        <input className='text-input' type="text" placeholder="Set a Password" />
-                        <button className='button'>Sign Me Up</button>
-                    </div>
-                </form>
+                <div className='hero__header-title'>
+                    <h1 className='h1'>Ready</h1>
+                    <h1 className='h1'>Set</h1>
+                    <h1 className='h1'>Code</h1>
+                </div>
             </div>
+            <div className='hero__message desktop'>
+                <p>Do you have the desire to build, disassemble, question, problem solve and conquer challenges that others cannot?</p>
+                <p>Sign up for our code challenges, receive a free Code Cavers notebook, and we will help get you to the starting line.</p>
+            </div>
+        </div><div className='hero__form-container'>
+            <div className='hero__message mobile'>
+                <p>Sign up for our code challenges, receive a free Code Cavers notebook, and we will help get you to the starting line.</p>
+            </div>
+            <form className='hero__form'>
+                <input className='text-input' type="text" placeholder="Name" />
+                <input className='text-input' type="text" placeholder="Email" />
+                <input className='text-input' type="text" placeholder="Set a Password" />
+                <button className='button'>Sign Me Up</button>
+            </form>
         </div>
     </div>
 ); 
