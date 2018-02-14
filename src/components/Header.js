7c7eaaac00070a449a93542f566df478135da824
Amy Plant <amy.plant@code-ninja.co>
2018-02-14 16:36:51 -0600

    Style Hero for both desktop and mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,10 +18,11 @@ export class Header extends React.Component {
                 <div className="header__content">
                     <Link className="header__logo" to='/'><img src="/images/logo_header.svg" /></Link>
                     <div className="header__navigation-desktop desktop">
-                        <NavLink className="header__link header__link--desktop" to='/Courses' activeClassName='is-active'>Courses</NavLink>
-                        <NavLink className="header__link header__link--desktop" to='/Team' activeClassName='is-active'>Our Team</NavLink>
-                        <NavLink className="header__link header__link--desktop" to='/Community' activeClassName='is-active'>Community</NavLink>
-                        <NavLink className="header__link header__link--desktop" to='/Contact' activeClassName='is-active'>Contact</NavLink>
+                        <NavLink className="header__link" to='/' exact={true} activeClassName='is-active'>Courses</NavLink>
+                        <NavLink className="header__link" to='/Team' activeClassName='is-active'>Our Team</NavLink>
+                        <NavLink className="header__link" to='/Community' activeClassName='is-active'>Community</NavLink>
+                        <NavLink className="header__link" to='/Contact' activeClassName='is-active'>Contact</NavLink>
+                        <button className="button" onClick={startLogin}>Login</button>
                     </div>
                     <div className="hamburger mobile"  onClick={this.onToggle}>
                         <div className="bar"></div>
@@ -30,10 +31,11 @@ export class Header extends React.Component {
                     </div>
                 </div>
                 <div id="nav-mobile" className="navlinks-mobile mobile">
-                    <NavLink className="header__link header__link--mobile" to='/Courses' activeClassName='is-active'>Courses</NavLink>
-                    <NavLink className="header__link header__link--mobile" to='/Team' activeClassName='is-active'>Our Team</NavLink>
-                    <NavLink className="header__link header__linke-mobile" to='/Community' activeClassName='is-active'>Community</NavLink>
-                    <NavLink className="header__link header__link--mobile" to='/Contact' activeClassName='is-active'>Contact</NavLink>
+                    <NavLink className="header__link" to='/' exact={true} activeClassName='is-active'>Courses</NavLink>
+                    <NavLink className="header__link" to='/Team' activeClassName='is-active'>Our Team</NavLink>
+                    <NavLink className="header__link" to='/Community' activeClassName='is-active'>Community</NavLink>
+                    <NavLink className="header__link" to='/Contact' activeClassName='is-active'>Contact</NavLink>
+                    <NavLink className="header__link" to='/'>Login</NavLink>
                 </div>
             </header>
         );
