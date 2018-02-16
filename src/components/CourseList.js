7d67c85c64c82336235e957767cb8a934aa40dc5
Amy Plant <amy.plant@code-ninja.co>
2018-02-16 11:38:03 -0600

    Refactor Course List to use component-base

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -2,10 +2,10 @@ import React from 'react';
 import CourseTile from './CourseTile';
 
 const CourseList = () => (
-    <div  className="course-list">
-        <div className='course-list__header'>
-            <h1 className='title'>Courses</h1>
-            <p className='subtitle'>
+    <div  className="component-base course-list">
+        <div className='component-base__header'>
+            <h1 className='component-base__title'>Courses</h1>
+            <p className='component-base__subtitle'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </p>
         </div>
