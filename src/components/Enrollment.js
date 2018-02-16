34c9effde2bc8d55415745fe67beec9feb56df65
Amy Plant <amy.plant@code-ninja.co>
2018-02-16 11:53:21 -0600

    Refactor Ernollment to use component-base

--- src/components/Enrollment.js
+++ src/components/Enrollment.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const EnrollmentCallout = () => (
-    <div  className="enrollment">
-        <h1>Considering Enrollment</h1>
-        <div className='enrollment-description'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    <div  className="component-base enrollment">
+        <div className='component-base__header'>
+            <h1 className='component-base__title'>Considering Enrollment</h1>
+            <p className='component-base__subtitle'>
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
         </div>
+
         <form className='enrollment-form'>
             <input className='text-input' placeholder='Name ... ' />
             <input className='text-input' placeholder='Email ... ' />
